ace796fc6021b064d3cee7d4a31e2187df645c1d
Odunayo Ogungbure <sogungbure@gmail.com>
2026-01-12 20:40:30 +0100

    Removing unused test

--- public/index.php
+++ public/index.php
@@ -17,4 +17,5 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+sleep(3);
 $app->handleRequest(Request::capture());
