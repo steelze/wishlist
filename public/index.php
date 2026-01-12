4e6d84f68221e722103e50f7fa0657dc67340553
Odunayo Ogungbure <sogungbure@gmail.com>
2026-01-12 20:45:01 +0100

    Remove sleep

--- public/index.php
+++ public/index.php
@@ -17,5 +17,4 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-sleep(3);
 $app->handleRequest(Request::capture());
